14c81a9e47679165a983006760090a7620a3c039
geourjoa <anthony.geourjon@gmail.com>
2017-03-14 14:37:02 +0100

    Paramétrisation du nombre de randonneurs

--- app/src/main/java/teamtreehouse/com/iamhere/MainActivity.java
+++ app/src/main/java/teamtreehouse/com/iamhere/MainActivity.java
@@ -1,13 +1,18 @@
 package teamtreehouse.com.iamhere;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,8 +36,37 @@ public class MainActivity extends AppCompatActivity {
         goToMapActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                SeekBar sb = (SeekBar) findViewById(R.id.seekBarNombreMembre);
+
+                SharedPreferences sharedPref = getSharedPreferences("MapsActivity", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt(getString(R.string.nbMembre), sb.getProgress());
+                editor.commit();
+
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        SeekBar sb = (SeekBar) findViewById(R.id.seekBarNombreMembre);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView tv = (TextView) findViewById(R.id.textViewNbMembre);
+                tv.setText(String.valueOf(seekBar.getProgress()));
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
---
+++ app/src/main/java/teamtreehouse/com/iamhere/UltraTeamApplication.java
@@ -0,0 +1,10 @@
+package teamtreehouse.com.iamhere;
+
+import android.app.Application;
+
+/**
+ * Created by anthony on 04/03/17.
+ */
+
+public class UltraTeamApplication extends Application {
+}
