a8b677a083517358a498e3e99ee1655c61c25d35
clement.rouquier <clement.rouquier@etu.univ-grenoble-alpes.fr>
2017-04-01 17:38:39 +0200

    Can now import group file from remote server. Useless but pretty. Creates a bug when launching map activity due to data struct, will correct in additional commit

--- app/src/main/java/teamtreehouse/com/iamhere/Groupe_Activity.java
+++ app/src/main/java/teamtreehouse/com/iamhere/Groupe_Activity.java
@@ -1,7 +1,13 @@
 package teamtreehouse.com.iamhere;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,16 +25,36 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import static teamtreehouse.com.iamhere.R.id.textView;
 
 public class Groupe_Activity extends AppCompatActivity {
     Button button_add_someone;
+    Button button_import_group;
+    Button button_create_group;
     String id_a_ajouter;
+    String file_url;
+    String group_id;
     ListView list;
 
     Membres membres= Membres.getInstance();
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    public static final int progress_bar_type = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +66,20 @@ public class Groupe_Activity extends AppCompatActivity {
         UltraTeamApplication.getInstance().setAdapter(new GroupeAdapter(Groupe_Activity.this, membres));
         UltraTeamApplication.getInstance().setGroupeInitialized(true);
         list.setAdapter(UltraTeamApplication.getInstance().getAdapter());
+
+        button_import_group = (Button) findViewById(R.id.import_group);
+        button_import_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) findViewById(R.id.name);
+                file_url = textView.getText().toString();
+
+
+                // execute this when the downloader must be fired
+                final DownloadTask downloadTask = new DownloadTask(Groupe_Activity.this);
+                downloadTask.execute("https://raw.githubusercontent.com/ultratrail/mobapp2/master/groupes/" + file_url + ".txt");
+            }
+        });
 
         button_add_someone = (Button) findViewById(R.id.Add_someone);
         button_add_someone.setOnClickListener(new View.OnClickListener() {
@@ -87,5 +127,102 @@ public class Groupe_Activity extends AppCompatActivity {
         InputMethodManager imm = (InputMethodManager) getSystemService(
                 Activity.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+    }
+
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+
+        public DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/" + file_url + ".txt");
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if (fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null)
+                Toast.makeText(context, "Download error: " + result, Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(context, "File downloaded", Toast.LENGTH_SHORT).show();
+
+            try {
+                InputStream instream = new FileInputStream(Environment.getExternalStorageDirectory().toString() + "/" + file_url + ".txt");
+                if (instream != null) {
+                    // prepare the file for reading
+                    InputStreamReader inputreader = new InputStreamReader(instream);
+                    BufferedReader buffreader = new BufferedReader(inputreader);
+                    UltraTeamApplication.getInstance().getAdapter().clear();
+                    String line;
+
+                    // read every line of the file into the line-variable, on line at the time
+                    do {
+                        line = buffreader.readLine();
+                        UltraTeamApplication.getInstance().getAdapter().add(line);
+                    } while (line != null);
+                    instream.close();
+                }
+            } catch (Exception ex) {
+                // print stack trace.
+            }
+        }
     }
 }
