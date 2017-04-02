cdbd49fab103ee02794cfa97aa19f5842a10a526
geourjoa <anthony.geourjon@gmail.com>
2017-04-02 18:56:08 +0200

    Page de démarrage amélioré

--- app/src/main/java/teamtreehouse/com/iamhere/MainActivity.java
+++ app/src/main/java/teamtreehouse/com/iamhere/MainActivity.java
@@ -51,14 +51,7 @@ public class MainActivity extends AppCompatActivity {
             registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
             BLUETOOTH_SERVICE_REGISTER = true;
         }
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.enavant);
 
